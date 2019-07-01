821dc770a64d49762ffc0816faa1e1f7679a73bd
Vasil Toshev <vasil.toshev@gmail.com>
2019-07-01 20:06:00 +0200

    readd folders

---
+++ loth-wedding-site/js/map.markers.js
@@ -0,0 +1,31 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Граждански брак",
+		"latitude":42.695535,
+		"longitude":23.323768,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"16:00 <br> Обредна зала Средец <br> ул. \"Леге\" 4"
+	},
+	{
+		"title": "Църковен брак",
+		"latitude":42.699204,
+		"longitude":23.330860,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"17:30 <br> Храм Света София <br> ул \"Париж\" 2"
+	},
+	{
+		"title": "Ресторант",
+		"latitude":42.676139,
+		"longitude":23.347008,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"18:30 <br> Ресторант Форест <br> 1113 Борисова Градина"
+	},
+	{
+		"title": "Хотел",
+		"latitude":42.6741459,
+		"longitude":23.3484383,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Парк Хотел Москва <br> ул. \"Незабравка\" 25"
+	},
+];
